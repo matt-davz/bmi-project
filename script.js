07f8bbb8f75648883e034cec00bff70ebe9820bd
matt-davz <davies.matt.02@gmail.com>
2023-07-12 18:10:20 +0700

    new bmi project commit

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+// Media query for limitation section tablet view//
+
+// function limitationTablet() {
+//     let screenWidth = window.innerWidth;
+//     if (screenWidth <= 1023 && screenWidth >= 767 ) { 
+//       genderTablet();
+//       muscleTablet();
+
+//     } else {
+//         genderWindow();
+//         muscleWindow();
+//     }
+// }
+
+
+// window.addEventListener("resize", limitationTablet);
+
+// window.onload = () =>{
+//     let screenWidth = window.innerWidth;
+//     if(screenWidth <= 1023 && screenWidth >= 767 ){
+//         genderTablet();
+//         muscleTablet();
+//     }
+// }
+
+// function muscleTablet() {
+//     let muscleDiv = document.getElementById("muscle");
+//     let muscleDiv2 = document.getElementById("muscle2");
+//     muscleDiv.style.display = "none";
+//     muscleDiv2.style.display = "block";
+// }
+
+// const muscleWindow = () => {
+//     let muscleDiv = document.getElementById("muscle");
+//     let muscleDiv2 = document.getElementById("muscle2");
+//     muscleDiv.style.display="block";
+//     muscleDiv2.style.display="none";
+// }
+
+// const genderTablet = () => {
+//     let genderDiv = document.getElementById("gender");
+//     let genderDiv2 = document.getElementById("gender2");
+//     genderDiv.style.display="none";
+//     genderDiv2.style.display="block";
+// }
+
+// const genderWindow = () => {
+//     let genderDiv = document.getElementById("gender");
+//     let genderDiv2 = document.getElementById("gender2");
+//     genderDiv.style.display="block";
+//     genderDiv2.style.display="none";
+// }
+
